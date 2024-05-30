5c949a0df2cd1646fa863af5e94b4bdc56666572
K9Creeper <nickklapal@gmail.com>
2024-05-30 00:14:33 -0400

    [Added] Animations to Sprite (Never tested)

--- People-Farm-Frenzy/game/graphics/animation/animation.h
+++ People-Farm-Frenzy/game/graphics/animation/animation.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <d3d9.h>
+#include <vector>
+#include <chrono>
+
+class Animation{
+public:
+	Animation() : textures{}, frameDelay{}{	}
+	Animation(std::vector<LPDIRECT3DTEXTURE9>textures, std::vector<std::chrono::milliseconds>frameDelay = {}) {
+		this->textures = textures;
+		this->frameDelay = frameDelay;
+
+		if (this->frameDelay.size() == 0) {
+			this->frameDelay.resize(this->textures.size());
+			for (int i = 0; i < this->textures.size(); i++)
+				this->frameDelay.push_back(std::chrono::milliseconds(17)); // Standard 60fps
+		}
+	}
+	~Animation() { textures.clear(); frameDelay.clear(); }
+	// Textures for each new animation frame
+	std::vector<LPDIRECT3DTEXTURE9>textures;
+	// Contains MS until next frame.
+	std::vector<std::chrono::milliseconds>frameDelay;
+};
--- People-Farm-Frenzy/game/graphics/sprite/sprite.h
+++ People-Farm-Frenzy/game/graphics/sprite/sprite.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <d3d9.h>
 
+#include "../animation/animation.h"
+
 struct Sprite {
 	// Origin
 	int x = 0, 
@@ -11,4 +13,51 @@ struct Sprite {
 		right = 0, bottom = 0;
 	
 	LPDIRECT3DTEXTURE9 texture = nullptr;
+	LPDIRECT3DTEXTURE9 textureOriginal = nullptr;
+
+	//	Plays Designated Animation
+	std::unordered_map<std::string, Animation>Animations;
+
+	bool inAnimation = false;
+	std::string curAnimation{};
+	int animationFrame = 0;
+	std::chrono::milliseconds tLastAnimationFrame = std::chrono::milliseconds(0);
+
+	void StartAnimation(const std::string& name) {
+		curAnimation.clear();
+		inAnimation = false;
+		animationFrame = 0;
+		tLastAnimationFrame = std::chrono::milliseconds(0);
+		if (Animations.find(name) != Animations.end())
+			return;
+		inAnimation = true;
+		curAnimation = name;
+
+		textureOriginal = texture;
+	}
+	
+	void UpdateAnimation() {
+		if (!inAnimation)
+			return;
+		Animation& animation = Animations[curAnimation];
+		if (animationFrame < animation.frameDelay.size() && (animationFrame == 0 || animation.frameDelay[animationFrame - 1] >= tLastAnimationFrame))
+		{
+			texture = animation.textures[animationFrame];
+			tLastAnimationFrame = std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::system_clock::now().time_since_epoch()
+				);
+			animationFrame++;
+		}
+		else if (animationFrame > animation.frameDelay.size()) {
+			StopAnimation();
+		}
+	}
+
+	void StopAnimation() {
+		inAnimation = false;
+		curAnimation.clear();
+		animationFrame = 0;
+		tLastAnimationFrame = std::chrono::milliseconds(0);
+		texture = textureOriginal;
+	}
 };
